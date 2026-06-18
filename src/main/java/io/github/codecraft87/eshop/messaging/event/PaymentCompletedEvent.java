4c76ee561e8fef23699f9059c5e0a550787e8645
Mahesh <maheshk.mca@gmail.com>
2026-06-18 12:20:54 +0530

    check in for order, payment, and basket outbox and consumer idempotency

--- src/main/java/io/github/codecraft87/eshop/messaging/event/PaymentCompletedEvent.java
+++
@@ -1,7 +0,0 @@
-package io.github.codecraft87.eshop.messaging.event;
-
-public record PaymentCompletedEvent(
-                        Long orderId, 
-                        String eventId) {
-
-}
--- src/main/java/io/github/codecraft87/eshop/messaging/event/PaymentFailedEvent.java
+++
@@ -1,6 +0,0 @@
-package io.github.codecraft87.eshop.messaging.event;
-
-public record PaymentFailedEvent(Long orderId, 
-                                   String eventId) {
-
-}
